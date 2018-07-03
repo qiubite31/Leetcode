92792c85449445851453eb5dcf18034b69d48680
qiubite31 <qiubite31@gmail.com>
2018-07-04 00:36:17 +0800

    18.07.04

---
+++ Hash/leetcode-349.py
@@ -0,0 +1,32 @@
+"""
+349. Intersection of Two Arrays
+Difficulty: Easy
+Related Topic: Hash Table
+
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
+"""
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
+        
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+
+solution = Solution()
+output = solution.intersection(nums1, nums2)
+
+print(output)
