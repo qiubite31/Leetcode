191f7dbafb074d97bfeb6746748b639e59851963
qiubite31 <qiubite31@gmail.com>
2018-08-10 17:40:03 +0800

    18.8.10

---
+++ LinkedList/leetcode-876.py
@@ -0,0 +1,60 @@
+"""
+876. Middle of the Linked List
+Difficulty: Easy
+Related Topic: Linked List
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+Example 2:
+
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        list_len = 0
+        node = head
+        while node is not None:
+            list_len += 1
+            node = node.next
+
+        tgt_num = int(list_len/2)+1
+
+        node = head
+        for i in range(1, tgt_num):
+            node = node.next
+
+        return node
+
+l = ListNode(1)
+l.next = ListNode(2)
+l.next.next = ListNode(3)
+l.next.next.next = ListNode(4)
+l.next.next.next.next = ListNode(5)
+
+solution = Solution()
+output = solution.middleNode(l)
+
+print(output)
